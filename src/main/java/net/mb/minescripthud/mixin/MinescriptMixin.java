412b1a57ee195e04412af6e557b661e78d2addcb
maksblack12 <maksymi.blue@gmail.com>
2026-06-16 16:58:50 +0200

    Added hover and click detection

--- src/main/java/net/mb/minescripthud/mixin/MinescriptMixin.java
+++ src/main/java/net/mb/minescripthud/mixin/MinescriptMixin.java
@@ -1,8 +1,12 @@
 package net.mb.minescripthud.mixin;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
 import net.mb.minescripthud.DrawHelper;
 import net.mb.minescripthud.MinescriptHUDAddon;
 import net.mb.minescripthud.ScriptFrameWaiter;
+import net.mb.minescripthud.util.MouseListener;
+import net.mb.minescripthud.util.MouseTracker;
 import net.minecraft.client.Minecraft;
 import net.minescript.common.*;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,14 +16,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Mixin(Minescript.class)
 public class MinescriptMixin {
-    @Inject(method = "runScriptFunction",at=@At("HEAD"),cancellable = true)
+	@Inject(method = "runScriptFunction",at=@At("HEAD"),cancellable = true)
 	@SuppressWarnings("unchecked")
-    private static void runScriptFunction(JobControl job, long funcCallId, ScriptFunctionCall functionCall, CallbackInfoReturnable<ScriptValue> cir) {
-        String name = functionCall.name();
-        ScriptFunctionCall.ArgList args = functionCall.args();
+	private static void runScriptFunction(JobControl job, long funcCallId, ScriptFunctionCall functionCall, CallbackInfoReturnable<ScriptValue> cir) {
+		String name = functionCall.name();
+		ScriptFunctionCall.ArgList args = functionCall.args();
 		switch (name) {
 			case "add_text" -> {
 				args.expectSize(7);
@@ -33,11 +38,6 @@ public class MinescriptMixin {
 				cir.setReturnValue(ScriptValue.of(id));
 				cir.cancel();
 			}
-			case "get_text_object" -> {
-				args.expectSize(1);
-				cir.setReturnValue(ScriptValue.of(DrawHelper.getInstance().getTextObject(args.getStrictInt(0))));
-				cir.cancel();
-			}
 
 
 
@@ -48,31 +48,16 @@ public class MinescriptMixin {
 				cir.setReturnValue(ScriptValue.of(id));
 				cir.cancel();
 			}
-			case "get_rectangle_object" -> {
-				args.expectSize(1);
-				cir.setReturnValue(ScriptValue.of(DrawHelper.getInstance().getRectangleObject(args.getStrictInt(0))));
-				cir.cancel();
-			}
 			case "add_gradient_rectangle" -> {
 				args.expectSize(8);
 				int id=DrawHelper.getInstance().addGradientRectangle(args.getStrictInt(0),args.getStrictInt(1),args.getStrictInt(2),args.getStrictInt(3),args.getStrictInt(4),args.getStrictInt(5),args.getDouble(6),args.getStrictInt(7));
 				cir.setReturnValue(ScriptValue.of(id));
 				cir.cancel();
 			}
-			case "get_gradient_rectangle_object" -> {
-				args.expectSize(1);
-				cir.setReturnValue(ScriptValue.of(DrawHelper.getInstance().getGradientRectangleObject(args.getStrictInt(0))));
-				cir.cancel();
-			}
 			case "add_stroked_rectangle" -> {
 				args.expectSize(7);
 				int id=DrawHelper.getInstance().addStrokedRectangle(args.getStrictInt(0),args.getStrictInt(1),args.getStrictInt(2),args.getStrictInt(3),args.getStrictInt(4),args.getDouble(5),args.getStrictInt(6));
 				cir.setReturnValue(ScriptValue.of(id));
-				cir.cancel();
-			}
-			case "get_stroked_rectangle_object" -> {
-				args.expectSize(1);
-				cir.setReturnValue(ScriptValue.of(DrawHelper.getInstance().getStrokedRectangleObject(args.getStrictInt(0))));
 				cir.cancel();
 			}
 
@@ -92,11 +77,6 @@ public class MinescriptMixin {
 				cir.setReturnValue(ScriptValue.of(id));
 				cir.cancel();
 			}
-			case "get_text_with_background_object" -> {
-				args.expectSize(1);
-				cir.setReturnValue(ScriptValue.of(DrawHelper.getInstance().getTextWithBackgroundObject(args.getStrictInt(0))));
-				cir.cancel();
-			}
 
 
 
@@ -112,11 +92,6 @@ public class MinescriptMixin {
 				args.expectSize(10);
 				int id=DrawHelper.getInstance().addAdvancedItem(args.getString(0), args.getStrictInt(1), args.getStrictInt(2), args.getDouble(3), args.getStrictInt(4), args.getDouble(5), args.getDouble(6), args.getDouble(7), args.getDouble(8), args.getDouble(9));
 				cir.setReturnValue(ScriptValue.of(id));
-				cir.cancel();
-			}
-			case "get_item_object" -> {
-				args.expectSize(1);
-				cir.setReturnValue(ScriptValue.of(DrawHelper.getInstance().getItemObject(args.getStrictInt(0))));
 				cir.cancel();
 			}
 
@@ -136,11 +111,6 @@ public class MinescriptMixin {
 				cir.setReturnValue(ScriptValue.of(id));
 				cir.cancel();
 			}
-			case "get_texture_object" -> {
-				args.expectSize(1);
-				cir.setReturnValue(ScriptValue.of(DrawHelper.getInstance().getTextureObject(args.getStrictInt(0))));
-				cir.cancel();
-			}
 
 
 
@@ -156,11 +126,6 @@ public class MinescriptMixin {
 				args.expectSize(13);
 				int id=DrawHelper.getInstance().addAdvancedShape((List<Map<String, Double>>)args.get(0), args.getDouble(1), args.getStrictInt(2), args.getDouble(3), args.getDouble(4), args.getDouble(5), args.getDouble(6), args.getDouble(7));
 				cir.setReturnValue(ScriptValue.of(id));
-				cir.cancel();
-			}
-			case "get_shape_object" -> {
-				args.expectSize(1);
-				cir.setReturnValue(ScriptValue.of(DrawHelper.getInstance().getShapeObject(args.getStrictInt(0))));
 				cir.cancel();
 			}
 
@@ -185,27 +150,32 @@ public class MinescriptMixin {
 				cir.setReturnValue(ScriptValue.of(DrawHelper.getInstance().getElements((List<Double>)args.get(0))));
 				cir.cancel();
 			}
+			case "get_element" -> {
+				args.expectSize(1);
+				cir.setReturnValue(ScriptValue.of(DrawHelper.getInstance().getElement(args.getStrictInt(0))));
+				cir.cancel();
+			}
 			case "still_exists" -> {
 				args.expectSize(1);
 				cir.setReturnValue(ScriptValue.of(DrawHelper.getInstance().stillExists(args.getStrictInt(0))));
 				cir.cancel();
 			}
 			case "get_mouse" -> {
-				cir.setReturnValue(ScriptValue.of(DrawHelper.getInstance().getMouse()));
+				cir.setReturnValue(ScriptValue.of(MouseTracker.getInstance().toJsonable()));
 				cir.cancel();
 			}
 			case "get_font_height" -> {
 				cir.setReturnValue(ScriptValue.of(Minecraft.getInstance().font.lineHeight));
 				cir.cancel();
 			}
-            case "wait_next_frame" -> {
+			case "wait_next_frame" -> {
 				job.suspend();
 				ScriptFrameWaiter.getInstance().waitNextFrame(funcCallId, job::resume);
-                cir.setReturnValue(ScriptValue.TRUE);
-                cir.cancel();
-            }
+				cir.setReturnValue(ScriptValue.TRUE);
+				cir.cancel();
+			}
 		}
-    }
+	}
 
 	@Inject(method = "runNoReturnScriptFunction",at=@At("HEAD"),cancellable = true)
 	@SuppressWarnings("unchecked")
@@ -232,7 +202,7 @@ public class MinescriptMixin {
 				cir.cancel();
 			}
 			case "update_stroked_rectangle" -> {
-				args.expectSize(9);
+				args.expectSize(8);
 				DrawHelper.getInstance().updateStrokedRectangle(args.getStrictInt(0),args.getStrictInt(1),args.getStrictInt(2),args.getStrictInt(3),args.getStrictInt(4),args.getStrictInt(5),args.getDouble(6),args.getStrictInt(7));
 				cir.setReturnValue(true);
 				cir.cancel();
@@ -281,6 +251,29 @@ public class MinescriptMixin {
 			case "suppress_done_message" -> {
 				MinescriptHUDAddon.silent=true;
 				cir.setReturnValue(true);
+				cir.cancel();
+			}
+		}
+	}
+
+	@Inject(method = "runExternalScriptFunction",at=@At("HEAD"),cancellable = true)
+	private static void runExternalScriptFunction(Job.SubprocessJob job, long funcCallId, ScriptFunctionCall functionCall, CallbackInfoReturnable<Optional<JsonElement>> cir) {
+		String functionName = functionCall.name();
+		ScriptFunctionCall.ArgList args = functionCall.args();
+		switch (functionName) {
+			case "register_mouse_listener" -> {
+				int elementId=args.getStrictInt(0);
+				DrawHelper.getInstance().addMouseListener(elementId,new MouseListener(job));
+				cir.setReturnValue(Optional.of(new JsonPrimitive(funcCallId)));
+				cir.cancel();
+			}
+			case "start_mouse_listener" -> {
+				int elementId=args.getStrictInt(0);
+				long listenerId=args.getStrictLong(1);
+				MouseListener h=DrawHelper.getInstance().getMouseListener(elementId);
+				job.addOperation(listenerId,h);
+				h.start(funcCallId,listenerId);
+				cir.setReturnValue(Optional.empty());
 				cir.cancel();
 			}
 		}
---
+++ src/main/java/net/mb/minescripthud/util/MouseTracker.java
@@ -0,0 +1,77 @@
+package net.mb.minescripthud.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.MouseHandler;
+import net.minescript.common.Jsonable;
+import org.lwjgl.glfw.GLFW;
+import com.mojang.blaze3d.platform.Window;
+
+public class MouseTracker {
+	private static final MouseTracker INSTANCE=new MouseTracker();
+	private boolean isLeftDown=false;
+	private boolean isMiddleDown=false;
+	private boolean isRightDown=false;
+	private boolean leftClicked=false;
+	private boolean middleClicked=false;
+	private boolean rightClicked=false;
+	private double x=0;
+	private double y=0;
+	private boolean isCursorLocked=false;
+
+	private MouseTracker() {}
+
+	public static MouseTracker getInstance() {return INSTANCE;}
+
+	public JsonableMouse toJsonable() {
+		return new JsonableMouse(getInstance());
+	}
+
+	public double getX() {return x;}
+
+	public double getY() {return y;}
+
+	public boolean anyButtonClicked() {return leftClicked||middleClicked||rightClicked;}
+
+	public boolean isCursorLocked() {return isCursorLocked;}
+
+	public void update() {
+		MouseHandler m=Minecraft.getInstance().mouseHandler;
+		Window window=Minecraft.getInstance().getWindow();
+		x=m.getScaledXPos(window);
+		y=m.getScaledYPos(window);
+		isCursorLocked=m.isMouseGrabbed();
+
+		boolean left=GLFW.glfwGetMouseButton(window.handle(),GLFW.GLFW_MOUSE_BUTTON_LEFT)==GLFW.GLFW_PRESS;
+		leftClicked=left&&!isLeftDown;
+		isLeftDown=left;
+
+		boolean middle=GLFW.glfwGetMouseButton(window.handle(),GLFW.GLFW_MOUSE_BUTTON_MIDDLE)==GLFW.GLFW_PRESS;
+		middleClicked=middle&&!isMiddleDown;
+		isMiddleDown=middle;
+
+		boolean right=GLFW.glfwGetMouseButton(window.handle(),GLFW.GLFW_MOUSE_BUTTON_RIGHT)==GLFW.GLFW_PRESS;
+		rightClicked=right&&!isRightDown;
+		isRightDown=right;
+	}
+
+	public static class JsonableMouse extends Jsonable {
+		public double x;
+		public double y;
+		public boolean isLeftDown;
+		public boolean isMiddleDown;
+		public boolean isRightDown;
+		public boolean wasLeftJustClicked;
+		public boolean wasMiddleJustClicked;
+		public boolean wasRightJustClicked;
+		public JsonableMouse(MouseTracker mouse) {
+			this.x=mouse.x;
+			this.y=mouse.y;
+			this.isLeftDown=mouse.isLeftDown;
+			this.isMiddleDown=mouse.isMiddleDown;
+			this.isRightDown=mouse.isRightDown;
+			this.wasLeftJustClicked=mouse.leftClicked;
+			this.wasMiddleJustClicked=mouse.middleClicked;
+			this.wasRightJustClicked=mouse.rightClicked;
+		}
+	}
+}
